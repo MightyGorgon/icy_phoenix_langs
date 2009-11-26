9c0d4cb0f752a0b76d18521c661357334d663b1c
Luca Libralato <mightygorgon@mightygorgon.com>
2009-11-26 22:46:26 +0000

    Other small lang fixes
    git-svn-id: http://svn.assembla.com/svn/icyphoenix_langs@69 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/italian/language/lang_italian/lang_admin.php
+++ trunk/italian/language/lang_italian/lang_admin.php
@@ -2088,7 +2088,7 @@ $lang['BACK_TO_PREV'] = 'Torna alla pagina precedente';
 
 // Custom BBCodes - BEGIN
 $lang['BBCODES_CUSTOM_BBCODES' = 'BBCode Personalizzati';
-$lang['BBCODES_CUSTOM_BBCODES_EXPLAIN' = 'In questa sezione puoi creare dei BBCodes personalizzati.';
+$lang['BBCODES_CUSTOM_BBCODES_EXPLAIN' = 'In questa sezione puoi creare dei BBCodes personalizzati. Per poter utilizzare questi BBCode devi attivare <b>Abilita i BBCode personalizzati</b> in <b>Configurazione &raquo; Icy Phoenix &raquo; Messaggi</b>.';
 $lang['BBCODES_NO_BBCODES' = 'Nessun BBCode';
 $lang['BBCODES_NO_BBCODES_SEL' = 'Nessun BBCode selezionato';
 $lang['BBCODES_NO_BBCODES_INPUT' = 'Devi compilare almeno il Tag BBCode';
